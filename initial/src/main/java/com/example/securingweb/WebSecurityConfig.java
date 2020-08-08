9a97df1ce4e2e6c216f87b9a19642773dd7bbbd2
Jayasree Vadakeyil Natarajapillai <Jayasree.VadakeyilNatarajapillai@chase.com>
2020-08-08 16:33:41 -0400

    Built application

--- initial/src/main/java/com/example/securingweb/WebSecurityConfig.java
+++ initial/src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -9,26 +9,28 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.inMemoryAuthentication()
-                .withUser("user1")
-                .password("password1")
-                .roles("ADMIN")
-                .and()
-                .withUser("user2")
-                .password("password1")
-                .roles("USER");
-    }
+//    @Bean
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//
+//        auth.inMemoryAuthentication()
+//                .withUser("user1")
+//                .password("password1")
+//                .roles("ADMIN")
+//                .and()
+//                .withUser("user2")
+//                .password("password1")
+//                .roles("USER");
+//    }
 
 
     @Override
@@ -47,16 +49,33 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 //    @Bean
-//    public UserDetailsService userDetailService(){
+//    @Override
+//    public UserDetailsService userDetailsService() {
 //        UserDetails user =
-//                User.withUserDetails()
-//                    .username("user")
-//                    .password("pass")
-//                    .roles("USER")
-//                    .build();
+//                User.withDefaultPasswordEncoder()
+//                        .username("user")
+//                        .password("password")
+//                        .roles("USER")
+//                        .build();
 //
 //        return new InMemoryUserDetailsManager(user);
 //    }
 
+    /*                OR                        */
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .passwordEncoder(passwordEncoder())
+            .withUser("user").password(passwordEncoder().encode("password")).roles("USER")
+            .and()
+            .withUser("user1").password(passwordEncoder().encode("password1")).roles("USER");
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
